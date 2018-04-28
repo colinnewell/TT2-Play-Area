9b24f50f52713c07ae2000cec57e3de1681be642
Colin Newell <colin.newell@gmail.com>
2018-04-28 21:45:29 +0100

    Got it working

--- lib/TT2/Play/Area.pm
+++ lib/TT2/Play/Area.pm
@@ -3,12 +3,30 @@ package TT2::Play::Area;
 # ABSTRACT: Simple site to allow playing with TT2 syntax and built in plugins.
 
 use strictures 2;
+use Cpanel::JSON::XS;
 use Dancer2;
 
 our $VERSION = '0.001';
 
 get '/' => sub {
     template 'index';
+};
+
+post '/tt2' => sub {
+    my $tt   = body_parameters->{template};
+    my $vars = decode_json(body_parameters->{vars});
+
+    my $config = {};
+
+    # create Template object
+    my $template = Template->new($config);
+    my $output;
+    $template->process( \$tt, $vars, \$output )
+      || die $template->error();
+
+    send_as JSON => {
+        result   => $output,
+    };
 };
 
 1;

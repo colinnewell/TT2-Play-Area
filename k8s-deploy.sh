2ba34f0b2fe6da651755e393d465ae264eb45f2e
Colin Newell <colin.newell@gmail.com>
2019-04-08 20:21:27 +0100

    Wire up to traefik

--- k8s-deploy.sh
+++ k8s-deploy.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
+# use k3s if present
 K3S=$(which k3s)
 $K3S kubectl create secret generic tt2-playarea \
     --from-literal=secret="$(openssl rand -hex 32)"
 $K3S kubectl apply -f k8s-deployment.yml
-$K3S kubectl expose deployment tt2 --port=5000 --type=NodePort
+$K3S kubectl expose deployment tt2 --port=5000
+$K3S kubectl apply -f tt2-traefik.yml
